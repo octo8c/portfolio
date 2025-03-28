4a3e4310f1901d4d77708f9659dff203b155e2de
mercier <christophe.mercier@etu.u-paris.fr>
2025-03-28 01:30:15 +0100

    Version plus propre avec debut des encart

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    
+    $("div#presentation1").append(
+        $("<p>",{
+            text : "Premier projet : "
+        })
+    );
+    $("div#presentation2").append(
+        $("<p>",{
+            text : "Deuxieme projet : "
+        })
+    );
+    $("div#presentation3").append(
+        $("<p>",{
+            text : "Troisieme projet : "
+        })
+    );
+    $("div#presentation4").append(
+        $("<p>",{
+            text : "Quatrieme projet : "
+        })
+    );
 });
