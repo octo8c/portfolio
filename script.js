7ca45d23589d1fb2a7a24d3bb7a4c836b8d51507
mercier <christophe.mercier@etu.u-paris.fr>
2025-03-30 23:56:08 +0200

    essaie

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 $(document).ready(function () {
-    const express = require('express');
-    const file = require('fs');
-    const read = fs();
-    const app = express();
-    app.set("view engine", "ejs");
-    app.use(express.urlencoded({ extended: true }));
-
-    const projets = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-    var ind = 0;
-    function addEntreProjet(projet){
-        var content = "content"+ind;
-        var pres = "presentation"+ind;
+    fetch('./data.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log("AFFICHAGE")
+            // Traitez vos données ici
+        });
+    function addEntreProjet(projet) {
+        var content = "content" + ind;
+        var pres = "presentation" + ind;
         $("#row-center").append(
-            '<div class="col-sm-6 order-sm-1" id='+content+'>' +
-                '<div class="jumbotron" id='+presentation+'>' +
-                    '<a href='+projet.lien+'a>'+
-                    projet.titre + '\n' +
-                    projet.description +
-                    'a/>' +
-                '</div>'
-            +'</div>'
+            '<div class="col-sm-6 order-sm-1" id=' + content + '>' +
+            '<div class="jumbotron" id=' + presentation + '>' +
+            '<a href=' + projet.lien + 'a>' +
+            projet.titre + '\n' +
+            projet.description +
+            'a/>' +
+            '</div>'
+            + '</div>'
         );
         ind++;
     }
-    projets.map(projet =>{
+    /*projets.map(projet =>{
         addEntreProjet();
         console.log("Affichage "+ind);
 
-    });
-    
+    });*/
+
 });
