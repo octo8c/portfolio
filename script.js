f623c497a126a9f70b5e75e6e1239393669e721a
mercier <christophe.mercier@etu.u-paris.fr>
2025-03-30 23:30:45 +0200

    Seconde version

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-$(document).ready(function(){
-    $("div#presentation1").append(
-        $("<p>",{
-            text : "Premier projet : "
-        })
-    );
-    $("div#presentation2").append(
-        $("<p>",{
-            text : "Deuxieme projet : "
-        })
-    );
-    $("div#presentation3").append(
-        $("<p>",{
-            text : "Troisieme projet : "
-        })
-    );
-    $("div#presentation4").append(
-        $("<p>",{
-            text : "Quatrieme projet : "
-        })
-    );
+$(document).ready(function () {
+    const express = require('express');
+    const file = require('fs');
+    const read = fs();
+    const app = express();
+    app.set("view engine", "ejs");
+    app.use(express.urlencoded({ extended: true }));
+
+    const projets = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+    var ind = 0;
+    function addEntreProjet(projet){
+        var content = "content"+ind;
+        var pres = "presentation"+ind;
+        $("#row-center").append(
+            '<div class="col-sm-6 order-sm-1" id='+content+'>' +
+                '<div class="jumbotron" id='+presentation+'>' +
+                    '<a href='+projet.lien+'a>'+
+                    projet.titre + '\n' +
+                    projet.description +
+                    'a/>' +
+                '</div>'
+            +'</div>'
+        );
+        ind++;
+    }
+    projets.map(projet =>{
+        addEntreProjet();
+        console.log("Affichage "+ind);
+
+    });
+    
 });
